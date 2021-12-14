1923368c7850b077341bf78061ec339fbc703f09
Diogo Macedo <diogobr@gmail.com>
2021-12-14 12:44:23 -0300

    Criado componente card

--- angular-p01/src/app/app.component.ts
+++ angular-p01/src/app/app.component.ts
@@ -6,5 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-p01';
+
+  novoContato = {
+    nome: '',
+    email: ''
+  }
+
+  contatos: Array<any> = [];
+
+  salvarContato() {
+    console.log(this.novoContato);
+    this.contatos.push(this.getContato());
+    this.initContato();
+  }
+
+  removerContato(contato: any) {
+    let index = this.contatos.findIndex(item => item.nome == contato.nome && item.email == contato.email);
+    if (index >= 0) {
+      this.contatos.splice(index, 1);
+    }
+  }
+
+  private initContato() {
+    this.novoContato = {
+      nome: '',
+      email: ''
+    }
+  }
+
+  private getContato() {
+    return JSON.parse(JSON.stringify(this.novoContato));
+  }
+
 }
